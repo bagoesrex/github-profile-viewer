2ab8673ab2f0665307f0e09819aed451085ae596
bagoesrex <bagoesrex@gmail.com>
2025-11-22 21:30:31 +0700

    chore: configure global tanstack query options

--- lib/query-client.ts
+++ lib/query-client.ts
@@ -1,3 +1,12 @@
 import { QueryClient } from "@tanstack/react-query";
 
-export const queryClient = new QueryClient({});
+export const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: false,
+            refetchOnWindowFocus: false,
+            refetchOnReconnect: false,
+            refetchInterval: false,
+        }
+    }
+});
