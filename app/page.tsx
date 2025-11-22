062047c3c7073e010a71ab6c249535857148f517
bagoesrex <bagoesrex@gmail.com>
2025-11-22 18:48:01 +0700

    chore: initial commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <main className="min-h-screen w-full flex justify-center items-center">
+      <h1 className="text-xl font-bold">Github Profile Viewer</h1>
+    </main>
+  );
+}
